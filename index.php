c9cafbc9b32c41140c2e1ee43fdd6256f8142a43
romzyyy <fijay0117@gmail.com>
2023-01-07 18:43:57 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="navbar">
+        <div class="navbrand">
+            <h2>Perpustakaan</h2>
+        </div>
+        <div class="navlist">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#profile">Profile</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="thumbnail">
+        <div class="thumb-text">
+            <h1>Library</h1>
+            <h2>Universitay Bahaudin Mudhary Madura</h2>
+        </div>
+        </svg>
+    </div>
+    <div class="card" id="profile">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#2C3639" fill-opacity="1"
+                d="M0,224L120,197.3C240,171,480,117,720,128C960,139,1200,213,1320,250.7L1440,288L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z">
+            </path>
+            <div class="cardName">
+                <div class="cardTitle">
+                    <h1>Porfile Librari</h1>
+                </div>
+            </div>
+            <div class="cardDesc">
+                <div class="desc">
+                    <h1>Perpustakaan UNIBA MADURA</h1>
+                    <p>Perpustakaan UNIVERSITAS BAHAUDIN MUDHARY MADURA yang diresmikan oleh Kepala Perpustakaan
+                        Nasional
+                        RI, Bapak Drs. Muhammad Syarif Bando, MM bersama Bapak Dr. Achsanul Qosasi, CSFA, CFrA. Selaku
+                        Ketua
+                        Yayasan Kudsiyah Bahaudin Mudhary. Perpustakaan UNIBA MADURA menyediakan buku sebanyak 8000
+                        Eksemplar dan komputer sebanyak 20 Unit, Perpustakaan UNIBA MADURA terdiri dari 2 macam yaitu
+                        perpustakaan konvensional dan digital. Perpustakaan UNIBA MADURA bekerjasa sama dengan
+                        Perpustakaan
+                        Nasional RI dan FPPTI JATIM.</p>
+                </div>
+                <div class="vimi">
+                    <div class="visi">
+                        <h1>VISI</h1>
+                        <p>Menjadi perpustakaan yang unggul dengan fasilitas yang lengkap, modern, dan mampu memberikan
+                            pelayanan terbaik kepada pemakai berbasis teknologi informasi dan komunikasi.</p>
+                    </div>
+                    <div class="misi">
+                        <h1>MISI</h1>
+                        <p>Menyediakan semua bentuk informasi yang sesuai dengan kurikulum yang berlaku di UNIBA MADURA
+                            Mengelola informasi agar bisa diakses oleh pengguna dengan mudah, cepat dan tepat
+                            Memberikan fasilitas yang memadai kepada pengguna agar dapat mewujudkan fungsi perpustakaan
+                            sebagai sarana bantu proses belajar mengajar dan penelitian
+                            Menyebarkan informasi secara efektif dan efisien</p>
+                    </div>
+                </div>
+
+            </div>
+        </svg>
+    </div>
+    <div class="footer">
+        <div class="footer-contact">
+            <h2>Library Uniba</h2>
+
+            <p>Jl. Raya Lenteng, Aredake, Batuan, Kabupaten Sumenep, Jawa Timur 69451</p>
+            <p>Mobile : 087810103010</p>
+            <p>Email: perpustakaan@unibamadura.ac.id</p>
+        </div>
+        <div class="footerName">
+            <p>made with &hearts; by thirteen</p>
+        </div>
+    </div>
+</body>
+
+</html>
